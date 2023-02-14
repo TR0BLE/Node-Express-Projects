c90500a9813b754a23b38f8319b7527f393436f5
Prabal <prabaljain3@gmail.com>
2023-02-15 02:33:06 +0530

    Added documentation

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -9,6 +9,10 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+const swagger = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 //connect DB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
@@ -27,6 +31,10 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs-API </h1><a href="/api-docs">Documentation</a>')
+})
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocument))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
